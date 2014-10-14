1ea296218216749fac864e8de439d3d3c0767ccc
Jacques L. <j@kamicode.com>
2014-10-14 10:57:51 -0400

    init commit from staging

---
+++ administrator/components/com_akeeba/akeeba/serverkey.php
@@ -0,0 +1,1 @@
+<?php defined('AKEEBAENGINE') or die(); define('AKEEBA_SERVERKEY', 'Njc3ZGVjOGUxNGY4NGU1MDkwMjFiNTdmMDM0YjFmMjE='); ?>
---
+++ logs/joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-07-29 20:08:44 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-07-29T20:08:44+00:00	INFO	update	Update started by user Super User (6). Old version is 3.3.1.
+2014-07-29T20:08:45+00:00	INFO	update	Downloading update file from .
+2014-07-29T20:08:48+00:00	INFO	update	File Joomla_3.3.x_to_3.3.3-Stable-Patch_Package.zip successfully downloaded.
+2014-07-29T20:08:48+00:00	INFO	update	Starting installation of new version.
+2014-07-29T20:09:00+00:00	INFO	update	Finalising installation.
+2014-07-29T20:09:00+00:00	INFO	update	Deleting removed files and folders.
+2014-07-29T20:09:11+00:00	INFO	update	Cleaning up after installation.
+2014-07-29T20:09:11+00:00	INFO	update	Update to version 3.3.3 is complete.
